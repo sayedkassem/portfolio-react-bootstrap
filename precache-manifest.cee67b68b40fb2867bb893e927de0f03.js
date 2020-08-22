2598e80cf61cdc2c0dec5ab30a2102a698ceb278
Sayed Kassem <sayedkassem-9@outlook.com>
2020-08-22 14:23:39 +0300

    Updates

---
+++ precache-manifest.cee67b68b40fb2867bb893e927de0f03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd90f8f58d6e818334b4af5216b5cb4",
+    "url": "/portfolio-react-bootstrap/index.html"
+  },
+  {
+    "revision": "83197d25e5887c7c2ffb",
+    "url": "/portfolio-react-bootstrap/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8f334d3aa76686b98213",
+    "url": "/portfolio-react-bootstrap/static/css/main.9f88596c.chunk.css"
+  },
+  {
+    "revision": "83197d25e5887c7c2ffb",
+    "url": "/portfolio-react-bootstrap/static/js/2.eb302ba5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react-bootstrap/static/js/2.eb302ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f334d3aa76686b98213",
+    "url": "/portfolio-react-bootstrap/static/js/main.f74ade3a.chunk.js"
+  },
+  {
+    "revision": "350715ad46cea597a300",
+    "url": "/portfolio-react-bootstrap/static/js/runtime-main.9fcf1bd3.js"
+  },
+  {
+    "revision": "697a75897564ad60fbbc6aa8e9796789",
+    "url": "/portfolio-react-bootstrap/static/media/WeatherApp.697a7589.png"
+  },
+  {
+    "revision": "1bded2d6401613eedb4f4263c877b4bb",
+    "url": "/portfolio-react-bootstrap/static/media/taskManager.1bded2d6.png"
+  }
+]);
